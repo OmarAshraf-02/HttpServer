49d93af0244d0450c60d51bb757aec27025397f4
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2024-06-08 01:38:56 +0300

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -1,79 +1,77 @@
+using System.Data.SqlTypes;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Text;
 
-// Uncomment this block to pass the first stage
-try
-{
-    int port = 4221;
-    TcpListener server = new TcpListener(IPAddress.Any, port);
+int port = 4221;
 
-    server.Start();
+TcpListener server = new TcpListener(IPAddress.Any, port);
+server.Start();
+
+while (true)
+{
+    TcpClient client = server.AcceptTcpClient();
+    ThreadPool.QueueUserWorkItem(new WaitCallback(HandleTcp), client);
+}
+
+static void HandleTcp(object tcpClientObject)
+{
+    Console.WriteLine("Connected");
+    TcpClient client = (TcpClient)tcpClientObject;
+    NetworkStream stream = client.GetStream();
 
     byte[] reqBuffer = new byte[1024];
     string reqData;
 
-    while (true)
+    int bytes = stream.Read(reqBuffer, 0, reqBuffer.Length);
+
+    // Basic responses
+    string successResponse = "HTTP/1.1 200 OK\r\n\r\n";
+    string notFoundResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+
+    while (bytes != 0)
     {
-        Console.WriteLine("Waiting for connection");
+        reqData = Encoding.UTF8.GetString(reqBuffer, 0, bytes);
+        string[] lines = reqData.Split("\r\n");
 
-        TcpClient client = server.AcceptTcpClient();
-        Console.WriteLine("Connected");
+        string requestLine = lines[0];
 
-        NetworkStream stream = client.GetStream();
+        string[] splitRequestLine = requestLine.Split(" ");
+        string path = splitRequestLine[1];
 
-        int bytes = stream.Read(reqBuffer, 0, reqBuffer.Length);
+        string responseString;
+        byte[] responseBytes;
 
-        // Basic responses
-        string successResponse = "HTTP/1.1 200 OK\r\n\r\n";
-        string notFoundResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
-
-        while (bytes != 0)
+        if (path == "/") // checks if 2nd argument in request line (aka. request target) is correct
         {
-            reqData = Encoding.UTF8.GetString(reqBuffer, 0, bytes);
-            string[] lines = reqData.Split("\r\n");
-
-            string requestLine = lines[0];
-
-            string[] splitRequestLine = requestLine.Split(" ");
-            string path = splitRequestLine[1];
-
-            string responseString;
-            byte[] responseBytes;
-
-            if (path == "/") // checks if 2nd argument in request line (aka. request target) is correct
-            {
-                responseString = successResponse;
-            }
-            else if (path.StartsWith("/echo/"))
-            {
-                string[] endpoint = splitRequestLine[1].Split("/");
-                responseString = $"HTTP/1.1 200 OK\r\n" +
-                                  "Content-Type: text/plain\r\n" +
-                                 $"Content-Length: {endpoint[2].Length}\r\n" + // endpoint[2] is /echo/endpoint[2]
-                                 $"\r\n{endpoint[2]}";
-            }
-            else if (path == "/user-agent")
-            {
-                string[] headersAndBody = lines[1..];
-                string headerValue = headersAndBody[1].Remove(0, 12); // Removes the header key 'User-Agent'
-
-                responseString = "HTTP/1.1 200 OK\r\n" +
-                                 "Content-Type: text/plain\r\n" +
-                                $"Content-Length: {headerValue.Length}\r\n" +
-                                $"\r\n{headerValue}";
-            }
-            else
-            {
-                responseString = notFoundResponse;
-            }
-
-            responseBytes = Encoding.UTF8.GetBytes(responseString);
-            stream.Write(responseBytes);
+            responseString = successResponse;
         }
+        else if (path.StartsWith("/echo/"))
+        {
+            string[] endpoint = splitRequestLine[1].Split("/");
+            responseString = $"HTTP/1.1 200 OK\r\n" +
+                              "Content-Type: text/plain\r\n" +
+                             $"Content-Length: {endpoint[2].Length}\r\n" + // endpoint[2] is /echo/endpoint[2]
+                             $"\r\n{endpoint[2]}";
+        }
+        else if (path == "/user-agent")
+        {
+            string[] headersAndBody = lines[1..];
+            string headerValue = headersAndBody[1].Remove(0, 12); // Removes the header key 'User-Agent'
+
+            responseString = "HTTP/1.1 200 OK\r\n" +
+                             "Content-Type: text/plain\r\n" +
+                            $"Content-Length: {headerValue.Length}\r\n" +
+                            $"\r\n{headerValue}";
+        }
+        else
+        {
+            responseString = notFoundResponse;
+        }
+
+        responseBytes = Encoding.UTF8.GetBytes(responseString);
+        stream.Write(responseBytes);
     }
 }
-catch (SocketException ex)
-{
-    Console.WriteLine($"SocketException: {ex}");
-}
+
