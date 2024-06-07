8dab8ddb918a579dcd909a6e6365309540017e2c
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2024-06-08 00:49:48 +0300

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -2,45 +2,78 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
-
 // Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
-server.Start();
-var socket = server.AcceptSocket(); // wait for client
-
-// Responses
-byte[] successResponse = Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
-byte[] notFoundResponse = Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n");
-
-// Request
-byte[] req = new byte[1024];
-socket.Receive(req); // Blocks until request is received
-
-string[] reqLines = Encoding.UTF8.GetString(req).Split("\r\n");
-
-string requestLine = reqLines[0];
-
-string[] splitRequestLine = requestLine.Split(" ");
-
-if (splitRequestLine[1] == "/") // checks if 2nd argument in request line (aka. request target) is correct
+try
 {
-    socket.Send(successResponse);
-}
-else if (splitRequestLine[1].StartsWith("/echo/"))
-{
-    string[] endpoint = splitRequestLine[1].Split("/");
-    string responseString = $"HTTP/1.1 200 OK\r\n" +
-                             "Content-Type: text/plain\r\n" +
-                            $"Content-Length: {endpoint[2].Length}\r\n" + // endpoint[2] is /echo/endpoint[2]
-                            $"\r\n{endpoint[2]}";
+    int port = 4221;
+    TcpListener server = new TcpListener(IPAddress.Any, port);
 
-    byte[] echoResponse = Encoding.UTF8.GetBytes(responseString);
-    socket.Send(echoResponse);
-}
-else
-{
-    socket.Send(notFoundResponse);
-}
+    server.Start();
 
+    byte[] reqBuffer = new byte[1024];
+    string reqData;
+
+    while (true)
+    {
+        Console.WriteLine("Waiting for connection");
+
+        TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("Connected");
+
+        NetworkStream stream = client.GetStream();
+
+        int bytes = stream.Read(reqBuffer, 0, reqBuffer.Length);
+
+        // Basic responses
+        string successResponse = "HTTP/1.1 200 OK\r\n\r\n";
+        string notFoundResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+
+        while (bytes != 0)
+        {
+            reqData = Encoding.UTF8.GetString(reqBuffer, 0, bytes);
+            string[] lines = reqData.Split("\r\n");
+
+            string requestLine = lines[0];
+
+            string[] splitRequestLine = requestLine.Split(" ");
+            string path = splitRequestLine[1];
+
+            string responseString;
+            byte[] responseBytes;
+
+            if (path == "/") // checks if 2nd argument in request line (aka. request target) is correct
+            {
+                responseString = successResponse;
+            }
+            else if (path.StartsWith("/echo/"))
+            {
+                string[] endpoint = splitRequestLine[1].Split("/");
+                responseString = $"HTTP/1.1 200 OK\r\n" +
+                                  "Content-Type: text/plain\r\n" +
+                                 $"Content-Length: {endpoint[2].Length}\r\n" + // endpoint[2] is /echo/endpoint[2]
+                                 $"\r\n{endpoint[2]}";
+            }
+            else if (path == "/user-agent")
+            {
+                string[] headersAndBody = lines[1..];
+                string headerValue = headersAndBody[1].Remove(0, 12); // Removes the header key 'User-Agent'
+
+                responseString = "HTTP/1.1 200 OK\r\n" +
+                                 "Content-Type: text/plain\r\n" +
+                                $"Content-Length: {headerValue.Length}\r\n" +
+                                $"\r\n{headerValue}";
+            }
+            else
+            {
+                responseString = notFoundResponse;
+            }
+
+            responseBytes = Encoding.UTF8.GetBytes(responseString);
+            stream.Write(responseBytes);
+        }
+    }
+}
+catch (SocketException ex)
+{
+    Console.WriteLine($"SocketException: {ex}");
+}
