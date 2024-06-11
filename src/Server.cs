2589f94f6e9e02039ef0d89dfe16a62703b877e1
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2024-06-11 05:35:43 +0300

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -2,100 +2,159 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-int port = 4221;
-
-TcpListener server = new TcpListener(IPAddress.Any, port);
-server.Start();
-
-while (true)
+class Program
 {
-    TcpClient client = server.AcceptTcpClient();
-    await Task.Run(() => HandleClient(client));
-}
+    private const int Port = 4221;
+    private static readonly string NotFoundResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+    private static readonly string SuccessResponse = "HTTP/1.1 200 OK\r\n\r\n";
+    private static readonly string CreatedResponse = "HTTP/1.1 201 Created\r\n\r\n";
+    private static readonly string DefaultDataDirPath = "/tmp/data/codecrafters.io/http-server-tester";
 
-static Task HandleClient(TcpClient tcpClient)
-{
-    Console.WriteLine("Connected");
-    NetworkStream stream = tcpClient.GetStream();
-
-    byte[] reqBuffer = new byte[1024];
-    string reqData;
-
-    int bytes = stream.Read(reqBuffer, 0, reqBuffer.Length);
-
-    // Basic responses
-    string successResponse = "HTTP/1.1 200 OK\r\n\r\n";
-    string notFoundResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
-
-    reqData = Encoding.UTF8.GetString(reqBuffer, 0, bytes);
-    string[] lines = reqData.Split("\r\n");
-
-    string requestLine = lines[0];
-
-    string[] splitRequestLine = requestLine.Split(" ");
-    string path = splitRequestLine[1];
-
-    string responseString = "";
-    byte[] responseBytes;
-
-    if (path == "/") // checks if 2nd argument in request line (aka. request target) is correct
+    private static async Task Main(string[] args)
     {
-        responseString = successResponse;
+        TcpListener server = new TcpListener(IPAddress.Any, Port);
+        server.Start();
+        Console.WriteLine($"Server started on port {Port}");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            await Task.Run(() => HandleRequest(client));
+        }
+
+        static Task HandleRequest(TcpClient tcpClient)
+        {
+            NetworkStream stream = tcpClient.GetStream();
+
+            byte[] reqBuffer = new byte[1024];
+            string reqData;
+            int bytes = stream.Read(reqBuffer, 0, reqBuffer.Length);
+            reqData = Encoding.UTF8.GetString(reqBuffer, 0, bytes);
+
+            string[] lines = reqData.Split("\r\n");
+            string requestLine = lines[0];
+            string body = lines[lines.Length - 1];
+
+            string[] splitRequestLine = requestLine.Split(" ");
+            string httpVerb = splitRequestLine[0];
+            string path = splitRequestLine[1];
+
+            string responseString = "";
+            byte[] responseBytes;
+            StringBuilder responseBuilder = new StringBuilder();
+
+            if (httpVerb == "GET")
+            {
+                responseString = HandleGet(path, responseBuilder, lines, splitRequestLine);
+            }
+            else if (httpVerb == "POST")
+            {
+                responseString = HandlePost(path, splitRequestLine, body);
+            }
+
+            responseBytes = Encoding.UTF8.GetBytes(responseString);
+            stream.Write(responseBytes);
+            return Task.CompletedTask;
+        }
     }
-    else if (path == "/user-agent")
+
+    private static string HandlePost(string path, string[] splitRequestLine, string body)
+    {
+        if (path.StartsWith("/files/"))
+        {
+            string fileNameInRequestPath = splitRequestLine[1].Split("/")[2];
+            string filePath = DefaultDataDirPath + $"/{fileNameInRequestPath}";
+
+            using StreamWriter streamWriter = File.CreateText(filePath);
+            streamWriter.Write($"{body}");
+
+            return CreatedResponse;
+        }
+        else
+        {
+            return NotFoundResponse;
+        }
+
+    }
+
+    private static string HandleGet(string path, StringBuilder builder, string[] lines, string[] splitReqLine)
+    {
+        if (path == "/") // checks if 2nd argument in request line (aka. request target) is correct
+        {
+            return SuccessResponse;
+        }
+        else if (path == "/user-agent")
+        {
+            string headerValue = ExtractUserAgentHeader(lines);
+
+            builder.Append("HTTP/1.1 200 OK\r\n");
+            builder.Append("Content-Type: text/plain\r\n");
+            builder.Append($"Content-Length: {headerValue.Length}\r\n");
+            builder.Append($"\r\n{headerValue}");
+
+            return builder.ToString();
+        }
+        else if (path.StartsWith("/echo/"))
+        {
+            string[] endpoint = splitReqLine[1].Split("/");
+
+            builder.Append("HTTP/1.1 200 OK\r\n");
+            builder.Append("Content-Type: text/plain\r\n");
+            builder.Append($"Content-Length: {endpoint[2].Length}\r\n"); // endpoint[2] is /echo/endpoint[2]
+            builder.Append($"\r\n{endpoint[2]}");
+
+            return builder.ToString();
+        }
+        else if (path.StartsWith("/files/"))
+        {
+            string fNameInPath = splitReqLine[1].Split("/")[2];
+            string[] filePaths = Directory.GetFiles(DefaultDataDirPath);
+
+            return HandleFileRequest(filePaths, fNameInPath, builder);
+        }
+        else
+        {
+            return NotFoundResponse;
+        }
+    }
+
+    private static string ExtractUserAgentHeader(string[] lines)
     {
         string[] headersAndBody = lines[1..];
-        string headerValue = headersAndBody[1].Remove(0, 12); // Removes the header key 'User-Agent'
+        string headerValue = headersAndBody[1].Remove(0, 12); // Removes the header key 'User-Agent: '
 
-        responseString = "HTTP/1.1 200 OK\r\n" +
-                         "Content-Type: text/plain\r\n" +
-                        $"Content-Length: {headerValue.Length}\r\n" +
-                        $"\r\n{headerValue}";
+        return headerValue;
     }
-    else if (path.StartsWith("/echo/"))
-    {
-        string[] endpoint = splitRequestLine[1].Split("/");
-        responseString = $"HTTP/1.1 200 OK\r\n" +
-                          "Content-Type: text/plain\r\n" +
-                         $"Content-Length: {endpoint[2].Length}\r\n" + // endpoint[2] is /echo/endpoint[2]
-                         $"\r\n{endpoint[2]}";
-    }
-    else if (path.StartsWith("/files/"))
-    {
-        string dirPath = "/tmp/data/codecrafters.io/http-server-tester";
-        string fNameInPath = splitRequestLine[1].Split("/")[2];
-        string[] filePaths = Directory.GetFiles(dirPath);
 
-        foreach (string pth in filePaths)
+    private static string HandleFileRequest(string[] filePathArr, string requestedFile, StringBuilder builder)
+    {
+        string result = "";
+
+        foreach (string pth in filePathArr)
         {
-            Console.WriteLine(pth);
             string fName = Path.GetFileName(pth);
-            Console.WriteLine(fName);
-            if (fName == fNameInPath)
+            if (fName == requestedFile)
             {
                 FileInfo fInfo = new FileInfo(pth);
                 byte[] fContents = File.ReadAllBytes(fInfo.FullName);
                 string content = Encoding.UTF8.GetString(fContents);
 
-                responseString = $"HTTP/1.1 200 OK\r\n" +
-                                  "Content-Type: application/octet-stream\r\n" +
-                                 $"Content-Length: {fInfo.Length}\r\n" + // endpoint[2] is /echo/endpoint[2]
-                                 $"\r\n{content}";
+                builder.Append("HTTP/1.1 200 OK\r\n");
+                builder.Append("Content-Type: application/octet-stream\r\n");
+                builder.Append($"Content-Length: {fInfo.Length}\r\n");
+                builder.Append($"\r\n{content}");
+
+                result = builder.ToString();
+                break;
             }
         }
 
-        if (responseString == "")
+        if (result == "")
         {
-            responseString = notFoundResponse;
+            result = NotFoundResponse;
         }
-    }
-    else
-    {
-        responseString = notFoundResponse;
+
+        return result;
     }
 
-    responseBytes = Encoding.UTF8.GetBytes(responseString);
-    stream.Write(responseBytes);
-    return Task.CompletedTask;
 }
-
